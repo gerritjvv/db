216c52d070cb7648ca654d0cb814bd5125950e6f
gerritjvv <475713+gerritjvv@users.noreply.github.com>
2026-06-23 23:48:14 +0200

    add darwin

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
+echo 1
 OS="$(uname -s)"
 ARCH="$(uname -m)"
 
@@ -13,7 +13,7 @@ case "$OS" in
    exit 1
    ;;
 esac
-
+echo 2
 case "$ARCH" in
   x86_64) ARCH=amd64;;
   aarch64|arm64) ARCH=arm64;;
@@ -21,20 +21,23 @@ case "$ARCH" in
   echo "Unsupported architecture: $ARCH"
   exit 1
   ;;esac
+echo 3
 
 VERSION=$(curl -fsSL \
-  https://github.com/gerritjvv/db/releases/latest |
+  https://api.github.com/repos/gerritjvv/db/releases/latest|
   grep '"tag_name"' |
   cut -d '"' -f 4)
 
+echo 4 "$VERSION"
 FILE="db-${OS}-${ARCH}"
 
-curl -L \
+
+echo curl -L \
   -o db \
   "https://github.com/gerritjvv/db/releases/download/${VERSION}/${FILE}"
-
+echo 5
 chmod +x db
 
-sudo mv db /usr/local/bin/db
-
-echo "Installed $VERSION"
+#sudo mv db /usr/local/bin/db
+#
+#echo "Installed $VERSION"
